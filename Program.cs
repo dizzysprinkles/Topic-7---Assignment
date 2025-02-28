866fa3c257fe53553e3c4aae1358ea7a72b53b97
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-28 14:39:46 -0500

    Started Menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string choice = "";
+
+            while (choice != "q")
+            {
+                Console.Clear(); 
+                Console.WriteLine("Welcome to Topic 7's Main Menu!  Please select an option to try:");
+                Console.WriteLine();
+                Console.WriteLine("1 - List of Integers");
+                Console.WriteLine("2 - List of Strings");
+                Console.WriteLine("Q - Quit");
+                Console.WriteLine();
+                choice = Console.ReadLine().ToLower().Trim();
+                Console.WriteLine();
+
+                if (choice == "1")
+                {
+                    //Do option 1
+                    Console.WriteLine("You chose option 1 - List of Integers");
+                    Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+                }
+                else if (choice == "2")
+                {
+                    // Do option 2
+                    Console.WriteLine("You chose option 2 - List of Strings");
+                    Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+                }
+
+                else if
+
+                else
+                {
+                    Console.WriteLine("Invalid choice, press ENTER to continue.");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
